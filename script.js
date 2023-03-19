2cbe041c83ecda807b5df4f96e36375579a0efba
ievakupla <119410975+ievakupla@users.noreply.github.com>
2023-03-19 12:22:43 +0200

    Pievienoju selektorus html kodam

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let diena = 'otrdiena';
+const input1 =  document.querySelector(".js-teksts")
+const plusButton = document.querySelector(".js-plus")
 
-const vards = 'toms';
-
-console.log(diena)
+console.log(input1);
+console.log(plusButton);
